d564e2eda95a713f5982660a284f8075c8893c7a
Nacho <barrojojoaquin2@gmail.com>
2025-06-11 20:37:43 -0300

    agregue el maquetado de FormularioTarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import FormularioTarea from './components/FormularioTarea';
+import "bootstrap/dist/css/bootstrap.min.css";
+import FormularioTarea from "./components/FormularioTarea";
 function App() {
-
   return (
     <>
-    <main className='container my-4'>
-      <h1 className='text-center'>Lista de tareas</h1>
+      <main className="container my-4">
+        <h1 className="text-center">Lista de tareas</h1>
 
-      <FormularioTarea/>
-    </main>
-    <footer className='bg-dark text-light text-center py-3'>Todos los derechos reservados</footer>
+        <FormularioTarea />
+      </main>
+      <footer className="bg-dark text-light text-center py-3">
+        Todos los derechos reservados
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,9 +1,15 @@
+// import Button  from 'bootstrap';
+// import Form from 'react-bootstrap/Form';
+import {Form, Button} from 'react-bootstrap'
 const FormularioTarea = () => {
     return (
-        <div>
-
-        </div>
-    );
+    <Form>
+      <Form.Group className="mb-3 d-flex">
+        <Form.Control type="text" placeholder="Ingresa una tarea" />
+        <Button variant="info" >Enviar</Button>
+      </Form.Group>
+    </Form>
+  );
 };
 
 export default FormularioTarea;
