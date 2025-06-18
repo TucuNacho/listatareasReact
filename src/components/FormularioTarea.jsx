4fbe0e7efb88f5bbfea5f0974ab21fa5cd7b3b32
Nacho <barrojojoaquin2@gmail.com>
2025-06-18 20:16:51 -0300

    agregamos la tarea cargada por el usuario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -2,13 +2,32 @@
 // import Form from 'react-bootstrap/Form';
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
+
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("aqui deberia guardar la tarea");
+    //tomar la tarea que esta en el state de tareas y guardar en el state tareas (array)
+    setTareas([...tareas, tarea])
+    setTarea('')
+  };
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex">
-          <Form.Control type="text" placeholder="Ingresa una tarea" />
-          <Button variant="info">Enviar</Button>
+          <Form.Control
+            type="text"
+            placeholder="Ingresa una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button type="submit" variant="info">
+            Enviar
+          </Button>
         </Form.Group>
       </Form>
       <ListaTareas></ListaTareas>
