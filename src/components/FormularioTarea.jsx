c3fd3b3b6c36fa786ad173781268a444e0128cbd
Nacho <barrojojoaquin2@gmail.com>
2025-06-23 21:39:36 -0300

    agregamos validacion de campo para la tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,45 +1,62 @@
-// import Button  from 'bootstrap';
-// import Form from 'react-bootstrap/Form';
+import { useForm } from "react-hook-form";
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
 import { useState, useEffect } from "react";
 
 const FormularioTarea = () => {
-  const [tarea, setTarea] = useState("");
-  const tareaLocalStorage = JSON.parse(localStorage.getItem("ListaTareas")) || []
+  const tareaLocalStorage =
+    JSON.parse(localStorage.getItem("ListaTareas")) || [];
   const [tareas, setTareas] = useState(tareaLocalStorage);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("desde useEffect");
-    localStorage.setItem("ListaTareas", JSON.stringify(tareas))
-  },[tareas])
+    localStorage.setItem("ListaTareas", JSON.stringify(tareas));
+  }, [tareas]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const agregarTareas = (data) => {
     console.log("aqui deberia guardar la tarea");
+    console.log(data.inputTarea);
+
     //tomar la tarea que esta en el state de tareas y guardar en el state tareas (array)
-    setTareas([...tareas, tarea]);
-    setTarea("");
+    setTareas([...tareas, data.inputTarea]);
+    reset()
   };
 
   const borrarTarea = (nombretarea) => {
-    const tareasFiltradas= tareas.filter((item)=> item !== nombretarea)
-    setTareas(tareasFiltradas)
+    const tareasFiltradas = tareas.filter((item) => item !== nombretarea);
+    setTareas(tareasFiltradas);
   };
   return (
     <section>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit(agregarTareas)}>
         <Form.Group className="mb-3 d-flex">
           <Form.Control
             type="text"
             placeholder="Ingresa una tarea"
-            onChange={(e) => setTarea(e.target.value)}
-            value={tarea}
+            onChange={(e) => setTareas(e.target.value)}
+            {...register('inputTarea', {
+              required:'La tarea es un dato obligatorio',
+              minLength:{
+                value:3,
+                message: 'la tarea debe contener al menos 3 caracteres'
+              },
+              maxLength:{
+                value:50,
+                message:'la tarea solo debe contener 50 caracteres como maximo'
+              }
+            })}
           />
           <Button type="submit" variant="info">
             Enviar
           </Button>
         </Form.Group>
+        <Form.Text className="text-danger">{errors.inputTarea?.message}</Form.Text>
       </Form>
       <ListaTareas tareas={tareas} borrarTarea={borrarTarea}></ListaTareas>
     </section>
